c50fc729157a883e546c42f315cc432eb33e0c0c
DestCom <azevedo-a@hotmail.fr>
2022-02-08 18:30:39 +0100

    Work finish on the contact section

---
+++ client/src/assets/colors/Colors.js
@@ -0,0 +1,6 @@
+export const richblack = "#0d1321";
+export const prussianblue = "#1d2d44";
+export const darkelectricblue = "#3e5c76";
+export const shadowblue = "#748cab";
+export const eggshell = "#f0ebd8";
+export const floralwhite = "#f9f8f0";
